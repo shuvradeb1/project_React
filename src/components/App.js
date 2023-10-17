e0a0e12bcba1d6ab2d5ee5ca12c61092bb7fa34f
shuvradeb1 <shuvradeb67@gmail.com>
2023-10-18 00:06:28 +0600

    Result page created

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import "../Styles/App.css";
 import Layout from "./Layout";
-// import Home from "./pages/Home";
-  //  import Signup from "./pages/Signup";
-  //  import Login from "./pages/Login";
-  import Quiz from "./pages/Ouiz";
+//  import Home from "./pages/Home";
+    // import Signup from "./pages/Signup";
+    // import Login from "./pages/Login";
+  //  import Quiz from "./pages/Ouiz";
+   import Result from "./pages/Result";
 
 function App() {
   return (
    <Layout>
-    {/* <Home /> */}
+    {/* {<Home />} */}
     {/* { <Signup/> } */}
     {/* { <Login/> } */}
-    <Quiz />
+    {/* { {<Quiz />} } */}
+   { <Result />}
    </Layout>
    
   );
---
+++ src/components/pages/Result.js
@@ -0,0 +1,13 @@
+import Analysis from "../Analysis";
+import Summary from "../Summary";
+
+export default function Result(){
+    return(
+        <>
+        <Summary />
+        <Analysis />
+        
+        </>
+
+    );
+}
