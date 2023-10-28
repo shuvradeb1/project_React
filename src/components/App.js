fc9691b441a5ca77fa75532748b4d40b66ddcd82
shuvradeb1 <shuvradeb67@gmail.com>
2023-10-29 00:45:42 +0600

    Private_Router_setuped

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,6 @@ import {AuthProvider}from "../contexts/AuthContext";
 import PrivateRoute from "./PrivateRoute";
 
 
-
 function App() {
   return (
     <Router>
@@ -26,7 +25,6 @@ function App() {
           <Route exact path="/Result" Component={Result} />
           </Route>
 
-
         </Routes>
       </Layout>
       </AuthProvider>
@@ -35,58 +33,6 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-// import {BrowserRouter as Router,Routes, Route } from "react-router-dom";
-// import { AuthProvider } from "../contexts/AuthContext";
-// import "../Styles/App.css";
-// import Layout from "./Layout";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Quiz from "./pages/Quiz";
-// import Result from "./pages/Result";
-// import Signup from "./pages/Signup";
-// import PrivateRoute from "./PrivateRoute";
-// import PublicRoute from "./PublicRoute";
- 
-
-// function App() {
-//   return (
-//       <Router>
-//        <AuthProvider>
-//       <Layout>
-//         <Routes>
-//             <Route exact path="/" component={Home} />
-//             <PublicRoute exact path="/signup" component={Signup} />
-//             <PublicRoute exact path="/login" component={Login} />
-//             <PrivateRoute exact path="/quiz" component={Quiz} />
-//             <PrivateRoute exact path="/result" component={Result} />
-//             </Routes>
-//             </Layout>
-//       </AuthProvider>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
